ec5e6d4e91fdff876a0e3d3cce9540d292eeec4a
AnnLiz22 <bielakam@gmail.com>
2024-08-05 09:07:20 +0200

    headers removed from controller view

--- src/main/java/lietz/anna/demo/github/GitHubController.java
+++ src/main/java/lietz/anna/demo/github/GitHubController.java
@@ -3,9 +3,7 @@ package lietz.anna.demo.github;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -24,19 +22,12 @@ public class GitHubController {
         try {
             Map<Map<String, String>, Map<String, String>> repoBranchCommits = gitHubService.getRepositoriesWithBranches(username);
 
-            HttpHeaders headers = new HttpHeaders();
-            headers.setContentType(MediaType.APPLICATION_JSON);
-
             return ResponseEntity.ok()
-                    .headers(headers)
                     .body(repoBranchCommits);
 
         } catch (UserNotFoundException | IOException e) {
-            HttpHeaders headers = new HttpHeaders();
-            headers.setContentType(MediaType.APPLICATION_JSON);
 
             return ResponseEntity.status(HttpStatus.NOT_FOUND)
-                    .headers(headers)
                     .body(new ErrorResponse(HttpStatus.NOT_FOUND.value(),
                             "User " + username + " not found.")
                     );
