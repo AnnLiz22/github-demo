6c921afa073c31305302d1e5028b0d24b5d3421c
AnnLiz22 <bielakam@gmail.com>
2024-09-08 23:07:15 +0200

    demo app test class

---
+++ src/test/java/lietz/anna/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package lietz.anna.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
